a84e7bcb84748b3c5a96b64dd26cfe88d9cdcfef
Jonas <jonas.monnier@gmail.com>
2013-02-11 10:58:10 +0100

    Updated configs

--- index.conf.php
+++ index.conf.php
@@ -1,4 +1,8 @@
 <?php
 
 const APP_ID = '###############';
-const SECRET = '#################################';
+const APP_SECRET = '#################################';
+
+const APP_CANVAS_URL = 'https://apps.facebook.com/facebook-utils/';
+const APP_PAGE_TAB_URL = 'https://www.facebook.com/positronic.fr/?sk=app_473357126030652';
+const APP_WEBSITE_URL = 'http://positronic.fr/apps/facebook/facebook-utils/';
--- tab.conf.php
+++ tab.conf.php
@@ -1,5 +1,5 @@
 <?php
 
 const APP_ID = '###############';
-const SECRET = '#################################';
-const APP_TAB_URL = 'https://www.facebook.com/positronic.fr/?sk=app_148536015297053';
+const APP_SECRET = '#################################';
+const APP_TAB_URL = 'https://www.facebook.com/positronic.fr/?sk=app_314298428681424';
